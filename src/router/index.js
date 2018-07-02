c4b65747be1102695c4b146870c99b1722d9ea60
ivan <ivan.fedeveloper@gmail.com>
2018-07-02 15:03:43 +0800

    mod: add import pages

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Router from "vue-router"
 
 Vue.use(Router)
 
+// const Home = resolve =>
+//     import("../pages/home").then(module => resolve(module))
+
 export default new Router({
     routes: [
         {}
